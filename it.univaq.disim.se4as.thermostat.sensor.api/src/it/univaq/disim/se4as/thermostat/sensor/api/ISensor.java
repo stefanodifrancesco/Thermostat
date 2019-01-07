ad4f0ea5d0f49f786e47fa42d3c54702ecb1b5a5
fabio <fabio@10.171.56.126>
2019-01-07 11:35:18 +0100

    Sensor Api - Interface and model

---
+++ it.univaq.disim.se4as.thermostat.sensor.api/src/it/univaq/disim/se4as/thermostat/sensor/api/ISensor.java
@@ -0,0 +1,7 @@
+package it.univaq.disim.se4as.thermostat.sensor.api;
+
+public interface ISensor {
+
+	public SensorInfo getSensorInfo();
+	
+}
---
+++ it.univaq.disim.se4as.thermostat.sensor.api/src/it/univaq/disim/se4as/thermostat/sensor/api/SensorInfo.java
@@ -0,0 +1,23 @@
+package it.univaq.disim.se4as.thermostat.sensor.api;
+
+public class SensorInfo {
+	
+	private String sensorType;
+	private String room;
+	
+	
+	public String getSensorType() {
+		return sensorType;
+	}
+	public void setSensorType(String sensorType) {
+		this.sensorType = sensorType;
+	}
+	public String getRoom() {
+		return room;
+	}
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	
+	
+}
