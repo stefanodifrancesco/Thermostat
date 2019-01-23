b0bb2f62bb7d30f81d0c7aa84515bba74cbf0ec9
fabio <fabio@10.171.12.236>
2019-01-23 17:02:33 +0100

    SQL Manager - Added getPresenceHistory

--- it.univaq.disim.se4as.thermostat.SQLManager/src/it/univaq/disim/se4as/thermostat/SQLManager/SQLManager.java
+++ it.univaq.disim.se4as.thermostat.SQLManager/src/it/univaq/disim/se4as/thermostat/SQLManager/SQLManager.java
@@ -17,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.osgi.framework.BundleContext;
 
+import it.univaq.disim.se4as.thermostat.SQLManager.model.PresencePrediction;
 import it.univaq.disim.se4as.thermostat.SQLManager.model.SensedValue;
 
 public class SQLManager {
@@ -25,30 +26,23 @@ public class SQLManager {
 	private String database;
 	private String user;
 	private String password;
-	
-	public enum Interval{
-		LAST,
-		LAST_WEEK,
-		ALL
+
+	public enum Interval {
+		LAST, LAST_WEEK, ALL
 	}
-	
-	public enum DayOfWeek{
-		monday,
-		tuesday,
-		wednesday,
-		thursday,
-		friday,
-		saturday,
-		sunday
+
+	public enum DayOfWeek {
+		monday, tuesday, wednesday, thursday, friday, saturday, sunday
 	}
-	
+
 	public SQLManager(BundleContext context) {
 		setConfiguration(context);
 	}
 
 	public void setConfiguration(BundleContext context) {
 
-		System.out.println("SQLManager - Copy configuration files to " + context.getBundle().getDataFile("").getAbsolutePath());
+		System.out.println(
+				"SQLManager - Copy configuration files to " + context.getBundle().getDataFile("").getAbsolutePath());
 
 		// Get type of sensor and room and server URL
 		File configuration = context.getBundle().getDataFile("config.properties");
@@ -69,14 +63,14 @@ public class SQLManager {
 			if (configuration != null) {
 				input = new FileInputStream(configuration);
 				properties.load(input);
-				
+
 				this.server = properties.getProperty("server");
 				this.database = properties.getProperty("database");
 				this.user = properties.getProperty("user");
 				this.password = properties.getProperty("password");
-				
+
 			}
-			
+
 		} catch (IOException ex) {
 			System.out.println("IOException reading configuration file");
 		} finally {
@@ -95,19 +89,20 @@ public class SQLManager {
 		Connection connection = null;
 
 		try {
-			//Class.forName("com.mysql.jdbc.Driver");
+			// Class.forName("com.mysql.jdbc.Driver");
 
 			connection = DriverManager.getConnection(
 					"jdbc:mysql://" + server + "/" + database + "?" + "user=" + user + "&password=" + password + "");
 		} catch (Exception e) {
 			try {
-				connection = DriverManager.getConnection(
-						"jdbc:mysql://" + server + "/" + database + "?" + "user=" + user + "&password=" + password + "&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+				connection = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?" + "user="
+						+ user + "&password=" + password
+						+ "&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 		}
 		return connection;
 
@@ -119,8 +114,9 @@ public class SQLManager {
 		// PreparedStatements can use variables and are more efficient
 		PreparedStatement preparedStatement;
 		try {
-			preparedStatement = connection.prepareStatement("insert into se4as.sensed_values(room, sensor_type, value, timestamp) " +
-																						" values (?, ?, ?, NOW())");
+			preparedStatement = connection
+					.prepareStatement("insert into se4as.sensed_values(room, sensor_type, value, timestamp) "
+							+ " values (?, ?, ?, NOW())");
 
 			// Parameters start with 1
 			preparedStatement.setString(1, sensedValue.getRoom());
@@ -128,7 +124,7 @@ public class SQLManager {
 			preparedStatement.setDouble(3, sensedValue.getValue());
 
 			preparedStatement.executeUpdate();
-			
+
 			connection.close();
 
 		} catch (SQLException e) {
@@ -136,200 +132,221 @@ public class SQLManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public List<String> getRooms() {
 		List<String> rooms = new ArrayList<String>();
-		
+
 		Connection connection = connect();
 
 		// PreparedStatements can use variables and are more efficient
 		PreparedStatement preparedStatement;
-		
+
 		try {
-			preparedStatement = connection.
-					prepareStatement("select distinct room " + 
-									 " from sensed_values");
+			preparedStatement = connection.prepareStatement("select distinct room " + " from sensed_values");
 
 			ResultSet rs;
-			
-			 rs = preparedStatement.executeQuery();
-	            while ( rs.next() ) {
-	                String room = rs.getString("room");
-	                
-	                rooms.add(room);
-	            }
-	            connection.close();
-	            
-	            
-			
+
+			rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				String room = rs.getString("room");
+
+				rooms.add(room);
+			}
+			connection.close();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return rooms;
-		
+
 	}
-	
+
 	public List<String> getSensorTypes() {
 		List<String> sensorTypes = new ArrayList<String>();
-		
+
 		Connection connection = connect();
 
 		// PreparedStatements can use variables and are more efficient
 		PreparedStatement preparedStatement;
-		
+
 		try {
-			preparedStatement = connection.
-					prepareStatement("select distinct sensor_type " + 
-									 " from sensed_values");
+			preparedStatement = connection.prepareStatement("select distinct sensor_type " + " from sensed_values");
 
 			ResultSet rs;
-			
-			 rs = preparedStatement.executeQuery();
-	            while ( rs.next() ) {
-	                String sensorType = rs.getString("sensor_type");
-	                
-	                sensorTypes.add(sensorType);
-	            }
-	            connection.close();
-	            
-	            
-			
+
+			rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				String sensorType = rs.getString("sensor_type");
+
+				sensorTypes.add(sensorType);
+			}
+			connection.close();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return sensorTypes;
-		
+
 	}
 
-	
 	public List<SensedValue> getSensedData(String room, String sensorType, Interval internval) {
 		List<SensedValue> sensedValues = new ArrayList<SensedValue>();
-		
+
 		Connection connection = connect();
-		
+
 		String query;
 
 		// PreparedStatements can use variables and are more efficient
 		PreparedStatement preparedStatement;
 		try {
-			
-			
-			query = "SELECT timestamp, value, sensor_type, room " + 
-					" FROM se4as.sensed_values " +
-					"WHERE room = ? AND sensor_type = ? "
-					+ " ORDER BY timestamp DESC ";
-			
-			
+
+			query = "SELECT timestamp, value, sensor_type, room " + " FROM se4as.sensed_values "
+					+ "WHERE room = ? AND sensor_type = ? " + " ORDER BY timestamp DESC ";
+
 			switch (internval) {
 			case ALL:
-				
-				
-				
+
 				break;
-				
+
 			case LAST:
-				
-				query += " limit 0,1";	
-				
+
+				query += " limit 0,1";
+
 				break;
-				
+
 			case LAST_WEEK:
-				
+
 				query += " limit 0,10";
-				
+
 				break;
 
 			default:
 				break;
-			}			
-			
-			preparedStatement = connection.
-					prepareStatement(query);
-			
+			}
+
+			preparedStatement = connection.prepareStatement(query);
+
 			// Parameters start with 1
 			preparedStatement.setString(1, room);
 			preparedStatement.setString(2, sensorType);
-			
+
 			ResultSet rs;
-			
-			 rs = preparedStatement.executeQuery();
-	            while ( rs.next() ) {
-	            	SensedValue sensedValue = new SensedValue();
-	                sensedValue.setTimestamp(rs.getTimestamp("timestamp"));
-	                sensedValue.setSensorType(rs.getString("sensor_type"));
-	                sensedValue.setValue(rs.getDouble("value"));
-	                sensedValue.setRoom(rs.getString("room"));
-	                
-	                sensedValues.add(sensedValue);
-	                
-	            }
-	         
+
+			rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				SensedValue sensedValue = new SensedValue();
+				sensedValue.setTimestamp(rs.getTimestamp("timestamp"));
+				sensedValue.setSensorType(rs.getString("sensor_type"));
+				sensedValue.setValue(rs.getDouble("value"));
+				sensedValue.setRoom(rs.getString("room"));
+
+				sensedValues.add(sensedValue);
+
+			}
+
 			connection.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return sensedValues;
-		
+
 	}
 
 	public List<SensedValue> getPresenceData(String room, DayOfWeek dayOfWeek) {
 		List<SensedValue> sensedValues = new ArrayList<SensedValue>();
-		
+
 		Connection connection = connect();
-		
+
 		String query;
 
 		// PreparedStatements can use variables and are more efficient
 		PreparedStatement preparedStatement;
 		try {
-			
-			
-			query = "SELECT timestamp, value, sensor_type, room " + 
-					" FROM se4as.sensed_values " +
-					"WHERE sensor_type = 'presence' " +
-					" AND room = ? " +
-					" AND DAYNAME(timestamp) = ? " +
-					" AND timestamp BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW();" +
-					" ORDER BY timestamp DESC ";
-			
-			
-			preparedStatement = connection.
-					prepareStatement(query);
-			
+
+			query = "SELECT timestamp, value, sensor_type, room " + " FROM se4as.sensed_values "
+					+ "WHERE sensor_type = 'presence' " + " AND room = ? " + " AND DAYNAME(timestamp) = ? "
+					+ " AND timestamp BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW();" + " ORDER BY timestamp DESC ";
+
+			preparedStatement = connection.prepareStatement(query);
+
 			// Parameters start with 1
 			preparedStatement.setString(1, room);
 			preparedStatement.setString(2, dayOfWeek.toString());
-			
+
 			ResultSet rs;
-			
-			 rs = preparedStatement.executeQuery();
-	            while ( rs.next() ) {
-	            	SensedValue sensedValue = new SensedValue();
-	                sensedValue.setTimestamp(rs.getTimestamp("timestamp"));
-	                sensedValue.setSensorType(rs.getString("sensor_type"));
-	                sensedValue.setValue(rs.getDouble("value"));
-	                sensedValue.setRoom(rs.getString("room"));
-	                
-	                sensedValues.add(sensedValue);
-	                
-	            }
-	         
+
+			rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				SensedValue sensedValue = new SensedValue();
+				sensedValue.setTimestamp(rs.getTimestamp("timestamp"));
+				sensedValue.setSensorType(rs.getString("sensor_type"));
+				sensedValue.setValue(rs.getDouble("value"));
+				sensedValue.setRoom(rs.getString("room"));
+
+				sensedValues.add(sensedValue);
+
+			}
+
 			connection.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return sensedValues;
-		
+
 	}
-	
+
+	public List<PresencePrediction> getPresenceHistory(String room, DayOfWeek dayOfWeek) {
+		List<PresencePrediction> presencePredictions = new ArrayList<PresencePrediction>();
+
+		Connection connection = connect();
+
+		String query;
+
+		// PreparedStatements can use variables and are more efficient
+		PreparedStatement preparedStatement;
+		try {
+
+			query = "SELECT day, start, end, room " + "	FROM presences " + " WHERE day = ? and room = ? ";
+
+			preparedStatement = connection.prepareStatement(query);
+
+			// Parameters start with 1
+			preparedStatement.setString(1, room);
+			preparedStatement.setString(2, dayOfWeek.toString());
+
+			ResultSet rs;
+
+			rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				PresencePrediction presencePrediction = new PresencePrediction();
+				presencePrediction.setDay(rs.getString("day"));
+				presencePrediction.setStartTime(rs.getTimestamp("start"));
+				presencePrediction.setEndTime(rs.getTimestamp("end"));
+				presencePrediction.setRoom(rs.getString("room"));
+
+				presencePredictions.add(presencePrediction);
+
+			}
+
+			connection.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return presencePredictions;
+
+	}
+
 }
