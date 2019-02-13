31db24fc85a80a2b9bf132061b492deaacfb4db6
Stefano Di Francesco <stefano.dfr@outlook.it>
2019-02-13 22:54:12 +0100

    Update AnalysisThread.java

--- it.univaq.disim.se4as.thermostat.analyzer/src/it/univaq/disim/se4as/thermostat/analyzer/AnalysisThread.java
+++ it.univaq.disim.se4as.thermostat.analyzer/src/it/univaq/disim/se4as/thermostat/analyzer/AnalysisThread.java
@@ -3,7 +3,6 @@ package it.univaq.disim.se4as.thermostat.analyzer;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -72,8 +71,81 @@ public class AnalysisThread extends Thread {
 
 		return presencePredictions;
 	}
-
+	
 	private List<PresencePrediction> predictIntervals(List<SensedValue> values, String room, DayOfWeek day) {
+
+		List<PresencePrediction> intervalsPredictions = new ArrayList<PresencePrediction>();
+
+		Timestamp startTime = null;
+		Timestamp endTime = null;
+
+		Boolean presence = false;
+		Boolean first = false;
+		int firstDay = 0;
+		
+		PresencePrediction presencePredictiontemp = null;
+		
+		for (int i = values.size() - 1; i >= 0; i--) {
+
+			if (values.get(i).getValue() == 1 && presence == false) {
+				presence = true;
+
+				startTime = values.get(i).getTimestamp();
+			}
+
+			if (values.get(i).getValue() == 0 && presence == true && first == false) {
+				presence = false;
+				Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+				calendar.setTime(values.get(i).getTimestamp());
+				firstDay = calendar.get(Calendar.DAY_OF_YEAR);
+				first = true;
+				endTime = values.get(i).getTimestamp();
+
+				presencePredictiontemp = new PresencePrediction();
+				presencePredictiontemp.setRoom(room);
+				presencePredictiontemp.setDay(day.toString());
+				presencePredictiontemp.setStartTime(startTime);
+				presencePredictiontemp.setEndTime(endTime);
+
+				intervalsPredictions.add(presencePredictiontemp);
+			}
+
+			if (values.get(i).getValue() == 0 && first == true) {
+				Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+				calendar.setTime(values.get(i).getTimestamp());
+				int secondDay = calendar.get(Calendar.DAY_OF_YEAR);
+				
+				if (secondDay != firstDay) {
+					presence = false;
+					first = false;
+					endTime = values.get(i).getTimestamp();
+
+					presencePredictiontemp.setRoom(room);
+					presencePredictiontemp.setDay(day.toString());
+					presencePredictiontemp.setStartTime(startTime);
+					presencePredictiontemp.setEndTime(endTime);
+
+				}
+			}
+
+			if (values.get(i).getValue() == 1 && presence == true && i == 0) {
+
+				endTime = values.get(i).getTimestamp();
+
+				PresencePrediction presencePrediction = new PresencePrediction();
+				presencePrediction.setRoom(room);
+				presencePrediction.setDay(day.toString());
+				presencePrediction.setStartTime(startTime);
+				presencePrediction.setEndTime(endTime);
+
+				intervalsPredictions.add(presencePrediction);
+			}
+		}
+
+		return intervalsPredictions;
+	}
+
+	private List<PresencePrediction> todayIntervals(List<SensedValue> values, String room, DayOfWeek day) {
 
 		List<PresencePrediction> intervalsPredictions = new ArrayList<PresencePrediction>();
 
@@ -136,7 +208,7 @@ public class AnalysisThread extends Thread {
 				List<SensedValue> values = databaseAPI.getTodayPresenceIntervals(room);
 
 				if (values.size() > 0) {
-					todayPresence.addAll(predictIntervals(values, room, DayOfWeek.values()[numDay - 1]));
+					todayPresence.addAll(todayIntervals(values, room, DayOfWeek.values()[numDay - 1]));
 				}
 
 			
